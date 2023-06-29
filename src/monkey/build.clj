6fbff19a721438561a84815055419c13b086ebb8
Wout Neirynck <wout@monkey-projects.be>
2023-06-29 16:22:49 +0200

    Pom generation

--- src/monkey/build.clj
+++ src/monkey/build.clj
@@ -1,6 +1,7 @@
 (ns monkey.build
   "Opinionated functions for building Monkey Projects libraries"
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
 (def target "target")
 (def class-dir (str target "/classes"))
@@ -35,3 +36,28 @@
 (defn jar+install [args]
   (jar args)
   (install args))
+
+(defn pom
+  "Generates pom.xml file, with the given version"
+  [{:keys [version lib]}]
+  (let [basis (b/create-basis)
+        opts {:basis basis
+              :version version
+              :lib (symbol lib)
+              :target target}
+        pom "pom.xml"]
+    (println "Writing" pom)
+    (b/write-pom opts)
+    (b/copy-file {:src (str target "/" pom)
+                  :target pom})))
+
+(defn deploy
+  "Deploys the library to an external repo. If no repository is given,
+   it uses Clojars.  Be sure to set `CLOJARS_USERNAME` and `CLOJARS_PASSWORD`
+   in that case."
+  [{:keys [jar repository] :as opts}]
+  (pom opts)
+  (println "Deploying" jar "to" (or repository "clojars"))
+  (dd/deploy (merge {:installer :remote
+                     :artifact jar}
+                    opts)))
